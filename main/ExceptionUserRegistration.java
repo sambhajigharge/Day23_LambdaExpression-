cc7aa6557396faccce795dd3154ac5b820d18d27
sushant <sgharge299@gmail.com>
2022-08-04 20:41:33 +0530

    [sushant]implimented the test for password

--- main/ExceptionUserRegistration.java
+++ main/ExceptionUserRegistration.java
@@ -5,7 +5,6 @@ import java.util.regex.Pattern;
 
 public class ExceptionUserRegistration {
     public String regex;
-
     public boolean firstName(String firstName) {
         regex = "^[A-Z]{1}[a-z]{2,}$";
         Pattern pattern = Pattern.compile(regex);
@@ -17,6 +16,27 @@ public class ExceptionUserRegistration {
         regex = "^[A-Z]{1}[a-z]{2,}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(lastName);
+        return matcher.matches();
+    }
+
+    public boolean emailId(String emailIds) {
+        regex = "^[a-zA-Z-9]+([._+-]*[0-9A-Za-z]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-z]{2,4})?$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(emailIds);
+        return matcher.matches();
+    }
+
+    public boolean mobileNumber(String mobileNumber) {
+        regex = "^[0-9]{2}\\s{1}[0-9]{10}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(mobileNumber);
+        return matcher.matches();
+    }
+
+    public boolean passwordRule1(String password) {
+        regex = "^[a-z]{8,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
         return matcher.matches();
     }
 }
